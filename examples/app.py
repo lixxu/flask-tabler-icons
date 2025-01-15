e7795a48933a077d5f8f8b26ea7674cefecfdaa5
lix <xuzenglin@gmail.com>
2025-01-15 15:56:36 +0800

    update to 3.28.1

--- examples/app.py
+++ examples/app.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding=utf-8 -*-
 
+import importlib
+
 from flask import Flask, render_template
+
 from flask_tabler_icons import TablerIcons
 
 app = Flask(__name__)
@@ -10,4 +13,4 @@ TablerIcons(app)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", version=importlib.metadata.version("flask_tabler_icons"))
